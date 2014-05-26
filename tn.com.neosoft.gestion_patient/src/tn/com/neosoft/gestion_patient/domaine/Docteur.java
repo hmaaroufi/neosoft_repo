6e5bfab9242ee9acd20822db5b6865e9007235fe
hmaaroufi <hmaaroufi@hmaaroufi-pc>
2014-05-26 12:05:16 +0200

    class Docteur is added
    Signed-off-by: hmaaroufi <hmaaroufi@hmaaroufi-pc>

---
+++ tn.com.neosoft.gestion_patient/src/tn/com/neosoft/gestion_patient/domaine/Docteur.java
@@ -0,0 +1,15 @@
+package tn.com.neosoft.gestion_patient.domaine;
+
+public class Docteur extends User {
+
+	private String speciality;
+
+	public String getSpeciality() {
+		return speciality;
+	}
+
+	public void setSpeciality(String speciality) {
+		this.speciality = speciality;
+	}
+
+}
