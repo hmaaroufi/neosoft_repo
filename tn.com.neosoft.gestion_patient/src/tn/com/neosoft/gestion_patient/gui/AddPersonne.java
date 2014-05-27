5c2676641197091458aa94b94cdda8a8db87a862
hmaaroufi <hmaaroufi@172.16.75.115>
2014-05-27 09:47:04 +0200

    Signed-off-by: hmaaroufi <hmaaroufi@172.16.75.115>

--- tn.com.neosoft.gestion_patient/src/tn/com/neosoft/gestion_patient/gui/AddPersonne.java
+++ tn.com.neosoft.gestion_patient/src/tn/com/neosoft/gestion_patient/gui/AddPersonne.java
@@ -42,6 +42,7 @@ public class AddPersonne extends JFrame {
 	 * Create the frame.
 	 */
 	public AddPersonne() {
+		managementPatient =new ManagementPatient();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
